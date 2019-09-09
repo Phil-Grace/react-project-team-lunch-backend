1acdcb280fce21ece66d5c1dba6235c22d775b32
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-09-09 13:45:14 -0500

    fix stuff, hopefully won't break it

--- app/models/user_team.rb
+++ app/models/user_team.rb
@@ -2,5 +2,5 @@ class UserTeam < ApplicationRecord
     belongs_to :team
     belongs_to :user
     validates :user_id, :team_id, presence: true
-    validates :user_id, :team_id, uniqueness: true
+    # validates :user_id, :team_id, uniqueness: true
 end
